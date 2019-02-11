0dd98b29df3cc35e35dddde9ded5200cd9243d3c
Chris Grayston <chris.grayston95@gmail.com>
2019-02-11 13:30:35 -0700

    Initial Commit

---
+++ Journal-C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Journal-C
+//
+//  Created by Chris Grayston on 2/11/19.
+//  Copyright © 2019 Chris Grayston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Journal-C/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Journal-C
+//
+//  Created by Chris Grayston on 2/11/19.
+//  Copyright © 2019 Chris Grayston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
